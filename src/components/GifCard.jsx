a023e44eff80645a68a11695a201356cc86df128
jons-repo <s.jonathan4252@gmail.com>
2023-06-17 19:51:26 -0400

    Implemented GifCard that takes from props and renders one GIF

--- src/components/GifCard.jsx
+++ src/components/GifCard.jsx
@@ -1,2 +1,16 @@
 // Presentational component responible for rendering one GIF
 // Receives GIF information as props
+import React from "react";
+import SearchField from "./SearchField";
+
+const GifCard = (props) => {
+    const gif = props.gif
+
+    return (
+        <div>
+            <img src={gif.url} />
+        </div>
+    )
+};
+
+export default GifCard;

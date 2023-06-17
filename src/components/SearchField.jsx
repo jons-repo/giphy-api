4c8888a547c422d381e9f77bc8ba3edd5c4a94a2
jons-repo <s.jonathan4252@gmail.com>
2023-06-17 19:23:44 -0400

    Added filter dropdown and API calls based on user inputs

--- src/components/SearchField.jsx
+++ src/components/SearchField.jsx
@@ -1,19 +1,47 @@
 // Handles user input
 // Update search term state and triggers API request when searching
-// We pass the search term to parent App component using a callback function
+// We pass the search term to parent AppComponent using a callback function
 import React, { useState } from "react";
+import axios from 'axios';
 
 const SearchField = () => {
     const [searchName, setSearchName] = useState("");
+    const [searchFilter, setSearchFilter] = useState("regular");
+    const [gifs, setGifs] = useState("");
 
     // Updates the search name with event from input tag
     const handleInputChange = (event) => {
         setSearchName(event.target.value);
     }
 
+    const selectFilter = (event) => {
+        setSearchFilter(event.target.value);
+    }
+
+    async function fetchGifs() {
+        try {
+            // Getting different API links based on filter user selcted
+            if (searchFilter == "regular") {
+                let fetchedGifs = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${searchName}&api_key=GWNHM0RflUo45K45RKCCfjSTh8WxWzMT`);
+                setGifs(fetchedGifs.data);
+            }
+            else {
+                let fetchedGifs = await axios.get(`http://api.giphy.com/v1/gifs/${searchFilter}?api_key=GWNHM0RflUo45K45RKCCfjSTh8WxWzMT`);
+                setGifs(fetchedGifs.data);
+            }
+        } catch(error) {console.log(error)};
+        console.log(gifs);
+    }
+
     return (
         <div>
             <input value={searchName} onChange={handleInputChange} />
+            <select value={searchFilter} onChange={selectFilter}>
+                <option value="regular">Regular</option>
+                <option value="trending">Trending</option>
+                <option value="random">Random</option>
+            </select>
+            <button onClick={fetchGifs}>Search Gifs</button>
         </div>
     );
 

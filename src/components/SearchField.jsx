98111b77d44602431e72a052636eb80891dc77f1
jons-repo <s.jonathan4252@gmail.com>
2023-06-17 16:23:20 -0400

    Added input functionality to SearchField.jsx and cleared clutter from App.js

--- src/components/SearchField.jsx
+++ src/components/SearchField.jsx
@@ -1,3 +1,22 @@
 // Handles user input
 // Update search term state and triggers API request when searching
 // We pass the search term to parent App component using a callback function
+import React, { useState } from "react";
+
+const SearchField = () => {
+    const [searchName, setSearchName] = useState("");
+
+    // Updates the search name with event from input tag
+    const handleInputChange = (event) => {
+        setSearchName(event.target.value);
+    }
+
+    return (
+        <div>
+            <input value={searchName} onChange={handleInputChange} />
+        </div>
+    );
+
+};
+
+export default SearchField;

0adbd7d6ebb2da1d54a84e30de4daa8d5def8b87
jons-repo <s.jonathan4252@gmail.com>
2023-06-17 20:23:24 -0400

    Implemented all gifs to show up from fetched data

--- src/components/AppComponent.jsx
+++ src/components/AppComponent.jsx
@@ -1,7 +1,24 @@
 // Manages fetched GIFs
 import React, { useState } from "react";
+import axios from 'axios';
 import GifCard from "./GifCard";
+import SearchField from "./SearchField";
 
 const AppComponent = () => {
     const [gifs, setGifs] = useState([]);
-}
+
+    // Update gifs to fetched data
+    const updateGifs = (fetchedGifs) => {
+        setGifs(fetchedGifs)
+    };
+
+    return (
+        <div>
+            <h2>Results:</h2>
+            <SearchField updateGifs={updateGifs} />
+            {gifs.map(gif => <GifCard key={gif.id} images={gif.images} />)}
+        </div>
+    );
+};
+
+export default AppComponent;

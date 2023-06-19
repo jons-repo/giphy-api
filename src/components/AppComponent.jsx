7c862f0923c9d2edf22e6750b3ccedaa1e443a6e
jons-repo <s.jonathan4252@gmail.com>
2023-06-19 14:50:29 -0400

    Added detailed comments and removed textbox if user selects trending or random because no other query is needed for that

--- src/components/AppComponent.jsx
+++ src/components/AppComponent.jsx
@@ -1,6 +1,5 @@
 // Manages fetched GIFs
 import React, { useState } from "react";
-import axios from 'axios';
 import GifCard from "./GifCard";
 import SearchField from "./SearchField";
 
@@ -9,14 +8,18 @@ const AppComponent = () => {
 
     // Update gifs to fetched data
     const updateGifs = (fetchedGifs) => {
+        console.log(fetchedGifs);
         setGifs(fetchedGifs)
     };
 
     return (
         <div>
+            {/* Render the SearchField component and pass the updateGifs callback */}
             <SearchField updateGifs={updateGifs} />
             <h2>Results:</h2>
-            {gifs.map(gif => <GifCard key={gif.id} images={gif.images} />)}
+            {/* Iterate over the gifs array and render a GifCard component for each gif */}
+            {gifs.map(gif => <GifCard key={gif.id} /* Assign a unique key to each GIF card component */
+                images={gif.images}  /* Pass the 'images' property of the GIF object as a prop*/ />)}
         </div>
     );
 };
